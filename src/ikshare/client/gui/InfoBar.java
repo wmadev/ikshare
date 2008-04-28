0a4bdc4688ea1f0055651ea50fc973708a75a0ff
ward@awosy.be <ward@awosy.be@4e41daa1-e546-0410-81e7-1762c9c1ad2f>
2008-04-28 15:36:14 +0000

    Code wa opgekuisd en vooral ingekort :p ma dien player verneukt et nu voorlopig. Zal morgn wel es zien, mss ergens andere plaatsen ofzo?

--- src/ikshare/client/gui/InfoBar.java
+++ src/ikshare/client/gui/InfoBar.java
@@ -1,5 +1,6 @@
 package ikshare.client.gui;
 
+import ikshare.client.ClientController;
 import java.awt.FlowLayout;
 import java.io.File;
 
@@ -15,137 +16,80 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
+public class InfoBar extends Composite implements TransferQueueListener {
 
-public class InfoBar extends Composite implements TransferQueueListener{
-    
-    private Label lblNrUpload, lblNrDownload, lblNrSharedFolders, lblNrSharedFiles;
-    private Composite cmpInformation;
-    private MP3Player mp3Player;
+    private Label lblNrUpload,  lblNrDownload,  lblNrSharedFolders,  lblNrSharedFiles;
+    private static String ICON_DOWN = "resources/icons/tp_down.png";
+    private static String ICON_UP = "resources/icons/tp_up.png";
+    private static String ICON_SHAREDFOLDERS = "resources/icons/info_folders.png";
+    private static String ICON_SHAREDFILES = "resources/icons/info_files.png";
 
-	private static String ICON_DOWN = "resources/icons/tp_down.png";
-
-	private static String ICON_UP = "resources/icons/tp_up.png";
-	private static String ICON_SHAREDFOLDERS = "resources/icons/hp_sharedfolders.png";
-	private static String ICON_SHAREDFILES = "resources/icons/hp_sharedfiles.png";
     /*
      * Sets the layout of the infobar in the shell right (1 vertical grid and 1 horizontal grid)
      * The labels gonna be committed by the method init.
      */
-    public InfoBar(Shell shell,int flags){
-        super(shell,flags);
-        //GridData gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 1,1 );
-        //gd.heightHint=50;
-        //this.setLayoutData(gd);
-        RowLayout rowLayout = new RowLayout();
-        rowLayout.marginLeft = 5;
-        rowLayout.marginTop = 2;
-        rowLayout.marginRight = 5;
-        rowLayout.marginBottom = 2;
-        rowLayout.spacing = 0;
-        this.setLayout(rowLayout);
-        this.init();
+    public InfoBar(Shell shell, int flags) {
+        super(shell, flags);
+        GridData gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
+        this.setLayoutData(gd);
+        this.setLayout(new RowLayout());
+        init();
         EventController.getInstance().addTransferQueueListener(this);
     }
-    
-    /*
-     * Initializes the infobar with 3 labels and their specific numbers
-     */    
+
     private void init() {
-    	initInformation();
-    	mp3Player = new MP3Player(this, SWT.FILL);
-       // mp3Player.setLayoutData(new RowData(300,20));
+        // Transfer part
+        new Label(this, SWT.NONE).setText("Transfers: ");
+
+        if (new File(ICON_DOWN).exists()) {
+            new Label(this, SWT.NONE).setImage(new Image(Display.getCurrent(), ICON_DOWN));
+        }
+        lblNrDownload = new Label(this, SWT.NONE);
+        lblNrDownload.setText("0");
+
+        if (new File(ICON_UP).exists()) {
+            new Label(this, SWT.NONE).setImage(new Image(Display.getCurrent(), ICON_UP));
+        }
+        lblNrUpload = new Label(this, SWT.NONE);
+        lblNrUpload.setText("0");
+
+        // Shared folder part
+        new Label(this, SWT.NONE).setText("|");
+        new Label(this, SWT.NONE).setText("Shared Files: ");
+        int[] info = ClientConfigurationController.getInstance().getSharedInformation();
+        if (new File(ICON_SHAREDFOLDERS).exists()) {
+            new Label(this, SWT.NONE).setImage(new Image(Display.getCurrent(), ICON_SHAREDFOLDERS));
+        }
+        lblNrSharedFolders = new Label(this, SWT.NONE);
+        lblNrSharedFolders.setText(String.valueOf(info[0]));
+
+        if (new File(ICON_SHAREDFILES).exists()) {
+            new Label(this, SWT.NONE).setImage(new Image(Display.getCurrent(), ICON_SHAREDFILES));
+        }
+        lblNrSharedFiles = new Label(this, SWT.NONE);
+        lblNrSharedFiles.setText(String.valueOf(info[1]));
+
+        // player
+        new MP3Player(this, SWT.NONE);
+
 
     }
 
-	private void initInformation() {
-        RowLayout rowLayout = new RowLayout();
-        rowLayout.wrap = false;
-        rowLayout.pack = false;
-        rowLayout.justify = false;
-        rowLayout.marginLeft = 5;
-        rowLayout.marginTop = 2;
-        rowLayout.marginRight = 5;
-        rowLayout.marginBottom = 2;
-        rowLayout.spacing = 0;
-		
-		cmpInformation = new Composite(this, SWT.FILL);
-		cmpInformation.setLayout(rowLayout);
-		cmpInformation.setLayoutData(new RowData(550,20));
-		
-    	RowData rwIcons = new RowData(20,20);
-    	RowData rwText = new RowData(30,20);
-    	
-        Label lblDownload = new Label(cmpInformation,SWT.FILL);
-		if(new File(ICON_DOWN).exists()){
-			Image icon = new Image(Display.getCurrent(), ICON_DOWN);
-			lblDownload.setImage(icon);
-		}
-		lblDownload.setLayoutData(rwIcons);
-        //lblDownload.setText(ClientConfigurationController.getInstance().getString("numberofdownloads")+":");
-        //GridData data=new GridData(SWT.LEFT, SWT.CENTER, false, true, 1, 1);
-        //lblDownload.setLayoutData(data);
-        
-        
-        lblNrDownload = new Label(cmpInformation,SWT.FILL);
-        lblNrDownload.setText("0");
-        lblNrDownload.setLayoutData(rwText);
-        //GridData data2=new GridData(SWT.END, SWT.CENTER, true, true, 1,1);
-        //data2.widthHint=80;
-        //lblNrDownload.setLayoutData(data2);
-        
-        Label lblUpload = new Label(cmpInformation,SWT.FILL);
-		if(new File(ICON_UP).exists()){
-			Image icon = new Image(Display.getCurrent(), ICON_UP);
-			lblUpload.setImage(icon);
-		}
-		lblUpload.setLayoutData(rwIcons);
-        //lblUpload.setText(ClientConfigurationController.getInstance().getString("numberofuploads")+":");
-        //data=new GridData(SWT.LEFT, SWT.CENTER, false, true, 1, 1);
-        //lblUpload.setLayoutData(data);
-        
-        lblNrUpload = new Label(cmpInformation,SWT.FILL);
-        lblNrUpload.setText("0");
-        lblNrUpload.setLayoutData(rwText);
-        //lblNrUpload.setLayoutData(data2);
-        
-        Label lblSharedFolders = new Label(cmpInformation,SWT.FILL);
-		if(new File(ICON_SHAREDFOLDERS).exists()){
-			Image icon = new Image(Display.getCurrent(), ICON_SHAREDFOLDERS);
-			lblSharedFolders.setImage(icon);
-		}
-		lblSharedFolders.setLayoutData(rwIcons);
+    public void activeDownloadsChanged() {
+        this.getDisplay().asyncExec(new Runnable() {
 
-        lblNrSharedFolders = new Label(cmpInformation,SWT.FILL);
-        lblNrSharedFolders.setText(""+ClientConfigurationController.getInstance().getSharedInformation()[0]);;
-        lblNrSharedFolders.setLayoutData(rwText);
+            public void run() {
+                lblNrDownload.setText(String.valueOf(PeerFacade.getInstance().getActiveDownloads()));
+            }
+        });
+    }
 
-        Label lblSharedFiles = new Label(cmpInformation,SWT.FILL);
-		if(new File(ICON_SHAREDFILES).exists()){
-			Image icon = new Image(Display.getCurrent(), ICON_SHAREDFILES);
-			lblSharedFiles.setImage(icon);
-		}
-		lblSharedFiles.setLayoutData(rwIcons);
+    public void activeUploadsChanged() {
+        this.getDisplay().asyncExec(new Runnable() {
 
-        lblNrSharedFiles = new Label(cmpInformation,SWT.FILL);
-        lblNrSharedFiles.setText(""+ClientConfigurationController.getInstance().getSharedInformation()[1]);;
-        lblNrSharedFiles.setLayoutData(rwText);
-	}
-
-	public void activeDownloadsChanged() {
-		this.getDisplay().asyncExec(
-				new Runnable() {
-					public void run() {
-						lblNrDownload.setText(String.valueOf(PeerFacade.getInstance().getActiveDownloads()));
-					}
-				});
-	}
-
-	public void activeUploadsChanged() {
-		this.getDisplay().asyncExec(
-				new Runnable() {
-					public void run() {
-						lblNrUpload.setText(String.valueOf(PeerFacade.getInstance().getActiveUploads()));
-					}
-				});
-	}
+            public void run() {
+                lblNrUpload.setText(String.valueOf(PeerFacade.getInstance().getActiveUploads()));
+            }
+        });
+    }
 }
