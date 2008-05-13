97624e10788181933340cf85e0a9a335fdecd592
jonaswille <jonaswille@4e41daa1-e546-0410-81e7-1762c9c1ad2f>
2008-05-13 14:01:40 +0000

    opgekuist

--- src/ikshare/domain/DownloadInformation.java
+++ src/ikshare/domain/DownloadInformation.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ikshare.domain;
 
-/**
- *
- * @author awosy
- */
 public class DownloadInformation {
     private String ip;
     private int port;
